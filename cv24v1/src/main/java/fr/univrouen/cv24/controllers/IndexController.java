2690b1bc29323c3c5091b42edd2a7cff0e3a8af8
amine <bachar321amineo@icloud.com>
2024-03-20 14:16:54 +0100

    ajout de maven.json

--- cv24v1/src/main/java/fr/univrouen/cv24/controllers/IndexController.java
+++ cv24v1/src/main/java/fr/univrouen/cv24/controllers/IndexController.java
@@ -1,8 +1,8 @@
 package fr.univrouen.cv24.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class IndexController {
 @GetMapping("/")
